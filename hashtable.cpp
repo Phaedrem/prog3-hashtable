c682b1a520c82eaf19e768d42ae5c4d584964ee7
Darren Bowers <darren.bowers@g.austincc.edu>
2022-03-28 12:45:58 -0500

    stubbed out and prototyped all methods

--- hashtable.cpp
+++ hashtable.cpp
@@ -3,3 +3,57 @@ Name: Darren Bowers
 Coding 06
 Purpose: Creating Hash tables of linked lists
 **********************/
+
+#include "hashtable.h"
+
+/*********************************
+CONSTRUCTORS / DESTRUCTORS
+*********************************/
+
+HashTable::HashTable(){
+    count = 0;
+    for(int i = 0; i < HASHTABLESIZE; i++){
+        hashtable[i] = nullptr;
+    }
+}
+
+HashTable::~HashTable(){
+    for(int i = 0; i < HASHTABLESIZE; i++){
+        if(hashtable[i]){
+            delete hashtable[i];
+        }
+    }
+}
+
+/*********************************
+PRIVATE
+*********************************/
+
+int HashTable::hash(int id){
+    return id % HASHTABLESIZE;
+}
+
+/*********************************
+PUBLIC
+*********************************/
+
+
+bool HashTable::insertEntry(int, string*){
+    return true;
+}
+
+bool HashTable::getData(int, Data*){
+    return true;
+}
+
+bool HashTable::removeEntry(int){
+    return true;
+}
+
+int HashTable::getCount(){
+    return count;
+}
+
+void HashTable::printTable(){
+
+}
--- hashtable.h
+++ hashtable.h
@@ -3,3 +3,38 @@ Name: Darren Bowers
 Coding 06
 Purpose: Creating Hash tables of linked lists
 **********************/
+
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <iostream>
+#include "linkedlist.h"
+
+#define HASHTABLESIZE 15
+
+using std::cout;
+using std::endl;
+
+class HashTable {
+
+public:
+    HashTable();
+    ~HashTable();
+    bool insertEntry(int, string*);
+    bool getData(int, Data*);
+    bool removeEntry(int);
+    int getCount();
+    void printTable();
+
+private:
+    int hash(int);
+
+    Data *hashtable[HASHTABLESIZE];
+    int count; 
+
+
+};
+
+
+
+#endif //HASH_TABLE_H
